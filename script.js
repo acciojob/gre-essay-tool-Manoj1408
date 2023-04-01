36e3f77481de54881c208a7203e08afa11917608
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-04-01 13:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+const data = document.getElementById("evaluatedText");
+
+ data.addEventListener("input", () => {
+   var textLn = data.value;
+   document.getElementById("wordCount").innerHTML =
+     getWordCount(textLn);
+ });
+
+ function getWordCount(str) {
+   return str.trim().split(/\s+/).length;
+ }
